5f40644dce93c85b67ccaec9b1da6d512add1c59
Sam Sinz <sam@sinz.fr>
2022-11-30 09:42:47 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+require("dotenv").config()
+require("./config/dbConfig")
+const express = require("express")
+const path = require("path")
+const cookieParser = require("cookie-parser")
+const logger = require("morgan")
+const cors = require("cors")
+
+const app = express()
+
+//? Services like render use something called a proxy and you need to add this to your server
+app.set("trust proxy", 1)
+app.use(logger("dev"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(
+	cors({
+		origin: [process.env.FRONTEND_URL],
+		credentials: true,
+	})
+)
+
+app.use("/api", require("./routes/index"))
+app.use("/api/auth", require("./routes/auth"))
+
+require("./error-handling/index")(app)
+
+module.exports = app
