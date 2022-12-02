82a8438af8abd1f73f794c1f675de8bee30b73a1
Sam Sinz <sam@sinz.fr>
2022-12-02 10:35:29 +0100

    add

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,35 @@
 const router = require("express").Router();
 const protectRoute = require("../middlewares/protectRoute");
-const User = require('./../models/User.model')
-const Partner = require('./../models/Partner.model')
-const Hook = require('./../models/Hook.model')
+const User = require("./../models/User.model");
+const Partner = require("./../models/Partner.model");
+const Hook = require("./../models/Hook.model");
+const Achievement = require('./../models/Achievement.model')
 
 router.get("/", (req, res, next) => {
-	res.send("Server is running... 🏃‍♂️");
+  res.send("Server is running... 🏃‍♂️");
 });
 
 router.get("/private", protectRoute, (req, res, next) => {
-	res.send("Protection passed !");
+  res.send("Protection passed !");
 });
 
-router.get('/dashboard', async (req,res, next)=> {
-	res.status(200).json(await User.find()
-	.populate({path: "partners", populate: {path: "hooks", model: "Hook"}}))
-})
+router.get("/dashboard", async (req, res, next) => {
+  res.status(200).json(await User.find().populate({ path: "partners", populate: { path: "hooks", model: "Hook" } }));
+});
+
+router.get("/dashboard/:userId", async (req, res, next) => {
+  const user = await User.findById(req.params.userId).populate({ path: "partners", populate: { path: "hooks", model: "Hook" } });
+  // console.log(user)
+  res.status(200).json(user);
+});
+
+router.get("/achievements/:userId", async (req, res, next) => {
+  const user = await User.findById(req.params.userId).populate("achievements");
+  const achievements = [];
+  for (let i = 0; i < user.achievements.length; i++) {
+    achievements.push(user.achievements[i]);
+  }
+  res.status(200).json(achievements);
+});
 
 module.exports = router;
