f2238689c3dcf136a584a6091455071697c72edf
Sam Sinz <sam@sinz.fr>
2022-12-06 21:09:40 +0100

    remove partner and hook

--- routes/partners.js
+++ routes/partners.js
@@ -13,18 +13,27 @@ const Achievement = require("../models/Achievement.model");
  *
  */
 
-router.post(
-  "/create",
-  isAuthenticated,
-  uploader.single("image"),
-  async (req, res, next) => {
-    let gain = 0;
+router.post("/create", isAuthenticated, uploader.single("image"), async (req, res, next) => {
+  let gain = 0;
 
-    const {
-      name,
-      age,
-      nationality,
-      comment,
+  const { name, age, nationality, comment, location, date, type, rating, duration, orgasm, protection } = req.body;
+
+  if (name === "" || age === "" || date === "" || type === "" || rating === "" || duration === "" || orgasm === "" || protection === "") {
+    res.status(400).json({ message: "I need some informations to work with here!" });
+  }
+
+  const image = req.file?.path;
+
+  if (orgasm) {
+    gain += 50;
+  }
+
+  if (protection) {
+    gain += 50;
+  }
+
+  try {
+    const createdHook = await Hook.create({
       location,
       date,
       type,
@@ -32,97 +41,79 @@ router.post(
       duration,
       orgasm,
       protection,
-    } = req.body;
+    });
 
-    if (
-      name === "" ||
-      age === "" ||
-      date === "" ||
-      type === "" ||
-      rating === "" ||
-      duration === "" ||
-      orgasm === "" ||
-      protection === ""
-    ) {
-      res
-        .status(400)
-        .json({ message: "I need some informations to work with here!" });
-    }
+    const user = await User.findById(req.payload.id).populate({ path: "partners", populate: { path: "hooks", model: "Hook" } });
 
-    const image = req.file?.path;
+    // checkAchievement({user})
 
-    if (orgasm) {
-      gain += 50
-    }
+    const onFire = await Achievement.findOne({ name: "On fire" });
 
-    if (protection) {
-      gain += 50
-    }
+    if (!onFire) {
+      const lastMonthHooks = user.partners
+        .reduce((acc, cur) => {
+          return [...acc, ...cur.hooks];
+        }, [])
+        .filter((hook) => hook.date > lastMonthDate);
 
-    try {
-      const createdHook = await Hook.create({
-        location,
-        date,
-        type,
-        rating,
-        duration,
-        orgasm,
-        protection,
-      });
+      if (lastMonthHooks.length >= 5) {
+        gain += 50;
 
-      const user = await User.findById(req.payload.id).populate({ path: "partners", populate: { path: "hooks", model: "Hook" } })
-
-      // checkAchievement({user})
-
-      const onFire = await Achievement.findOne({ name: "On fire" });
-
-      if (!onFire) {
-        const lastMonthHooks = user.partners
-          .reduce((acc, cur) => {
-            return [...acc, ...cur.hooks]
-          }, [])
-          .filter(hook => hook.date > lastMonthDate)
-
-        if (lastMonthHooks.length >= 5) {
-          gain += 50
-
-          await User.findByIdAndUpdate(req.payload.id, { $push: { achievements: onFire.id } })
-
-        }
-
+        await User.findByIdAndUpdate(req.payload.id, { $push: { achievements: onFire.id } });
       }
-
-      const lastMonthDate = new Date()
-      lastMonthDate.setMonth(lastMonthDate.getMonth() - 1)
-
-
-
-
-      const createdPartner = await Partner.create({
-        name,
-        age,
-        nationality,
-        comment,
-        image: image,
-        hooks: createdHook,
-      });
-
-
-
-      console.log(req.payload.id);
-      await User.findByIdAndUpdate(req.payload.id, {
-        $push: { partners: createdPartner },
-        $inc: { score: gain }
-      });
-
-      res.status(201).json({ message: "Partner created" });
-    } catch (error) {
-      if (error instanceof mongoose.Error.ValidationError) {
-        return res.status(400).json({ message: error.message });
-      }
-      res.status(500).json({ message: "Sweet, sweet 500." });
     }
+
+    const lastMonthDate = new Date();
+    lastMonthDate.setMonth(lastMonthDate.getMonth() - 1);
+
+    const createdPartner = await Partner.create({
+      name,
+      age,
+      nationality,
+      comment,
+      image: image,
+      hooks: createdHook,
+    });
+
+    console.log(req.payload.id);
+    await User.findByIdAndUpdate(req.payload.id, {
+      $push: { partners: createdPartner },
+      $inc: { score: gain },
+    });
+
+    res.status(201).json({ message: "Partner created" });
+  } catch (error) {
+    if (error instanceof mongoose.Error.ValidationError) {
+      return res.status(400).json({ message: error.message });
+    }
+    res.status(500).json({ message: "Sweet, sweet 500." });
   }
-);
+});
+
+router.delete("/:partnerId/delete/:hookId", isAuthenticated, async (req, res, next) => {
+  try {
+    await Partner.findByIdAndUpdate(req.params.partnerId, { $pullAll: { hooks: [{ _id: req.params.hookId }] } });
+    await Hook.findByIdAndRemove(req.params.hookId);
+    res.sendStatus(200);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:partnerId/delete", isAuthenticated, async (req, res, next) => {
+  try {
+
+    const partnerToDelete = await Partner.findById(req.params.partnerId);
+    const hookPromises = partnerToDelete.hooks.map((hook)=> {
+      return Hook.findByIdAndRemove(hook)
+    })
+    await Promise.all(hookPromises)
+    await Partner.findByIdAndRemove(req.params.partnerId)
+    res.sendStatus(200);
+
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
