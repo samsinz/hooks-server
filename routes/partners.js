82bb9334870eee971cedb4df5598cdbd0ee92add
jsarzi <justine.sarzi@epitech.eu>
2022-12-06 18:10:08 +0100

    points

--- routes/partners.js
+++ routes/partners.js
@@ -6,6 +6,7 @@ const Partner = require("../models/Partner.model");
 const User = require("../models/User.model");
 const uploader = require("../config/cloudinary");
 const jwt = require("jsonwebtoken");
+const Achievement = require("../models/Achievement.model");
 /**
  *
  * * All the routes are prefixed with `/api/partners`
@@ -17,6 +18,8 @@ router.post(
   isAuthenticated,
   uploader.single("image"),
   async (req, res, next) => {
+    let gain = 0;
+
     const {
       name,
       age,
@@ -48,6 +51,14 @@ router.post(
 
     const image = req.file?.path;
 
+    if (orgasm) {
+      gain += 50
+    }
+
+    if (protection) {
+      gain += 50
+    }
+
     try {
       const createdHook = await Hook.create({
         location,
@@ -59,6 +70,34 @@ router.post(
         protection,
       });
 
+      const user = await User.findById(req.payload.id).populate({ path: "partners", populate: { path: "hooks", model: "Hook" } })
+
+      // checkAchievement({user})
+
+      const onFire = await Achievement.findOne({ name: "On fire" });
+
+      if (!onFire) {
+        const lastMonthHooks = user.partners
+          .reduce((acc, cur) => {
+            return [...acc, ...cur.hooks]
+          }, [])
+          .filter(hook => hook.date > lastMonthDate)
+
+        if (lastMonthHooks.length >= 5) {
+          gain += 50
+
+          await User.findByIdAndUpdate(req.payload.id, { $push: { achievements: onFire.id } })
+
+        }
+
+      }
+
+      const lastMonthDate = new Date()
+      lastMonthDate.setMonth(lastMonthDate.getMonth() - 1)
+
+
+
+
       const createdPartner = await Partner.create({
         name,
         age,
@@ -67,9 +106,13 @@ router.post(
         image: image,
         hooks: createdHook,
       });
+
+
+
       console.log(req.payload.id);
       await User.findByIdAndUpdate(req.payload.id, {
         $push: { partners: createdPartner },
+        $inc: { score: gain }
       });
 
       res.status(201).json({ message: "Partner created" });
